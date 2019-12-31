a600e3c90cd5beeddc5c03c99097186d605b0f89
Chameera Dulanga <chameeradulanga87@gmail.com>
2019-12-31 12:41:07 +0530

    bug fix (#15)
    * bug fix
    
    * route file isolate
    
    * testing file

--- src/products/handler.ts
+++ src/products/handler.ts
@@ -1,5 +1,10 @@
-import { APIGatewayProxyHandler, APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
-import { handleRoutes, routeRegistrations } from './routes';
+import {
+  APIGatewayProxyHandler,
+  APIGatewayProxyEvent,
+  APIGatewayProxyResult,
+} from 'aws-lambda';
+import { routeRegistrations } from './routes';
+import { handleRoutes } from '../shared/util/routeHandler';
 
 // PRODUCTS
 export const product: APIGatewayProxyHandler = async (
@@ -8,6 +13,6 @@ export const product: APIGatewayProxyHandler = async (
 ) => {
   const { match } = handleRoutes(event, routeRegistrations);
   const action = match && match.getAction();
-  const result =  action && await action(event) || {};
+  const result = (action && (await action(event))) || {};
   return result as APIGatewayProxyResult;
 };
