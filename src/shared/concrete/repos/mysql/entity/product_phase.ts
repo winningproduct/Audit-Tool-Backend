1d8b29fbaa1748bf24e06467c492bb8915e4ffa3
Chameera Dulanga <ChameeraD@99x.lk>
2020-01-03 14:07:43 +0530

    huskey

--- src/shared/concrete/repos/mysql/entity/product_phase.ts
+++ src/shared/concrete/repos/mysql/entity/product_phase.ts
@@ -1,7 +1,7 @@
 import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 
 @Entity()
-export class Product_Phase {
+export class ProductPhase {
   @PrimaryGeneratedColumn()
   Id!: number;
   @Column()
