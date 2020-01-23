fc41c3b2dc1c256fe682089a5b71fe5d8b2e51bd
nir099 <a.m.s.nirmal.a@gmail.com>
2020-01-23 15:26:27 +0530

    fixing routing bug

--- src/shared/util/responseHandler.ts
+++ src/shared/util/responseHandler.ts
@@ -1,11 +1,6 @@
 import { APIGatewayProxyResult } from 'aws-lambda';
 
 export function ok(obj: APIGatewayProxyResult): APIGatewayProxyResult {
-  obj['headers'] = {
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Headers': 'Content-Type',
-    'Access-Control-Allow-Methods': 'OPTIONS,POST,GET',
-  };
   return obj;
 }
 
